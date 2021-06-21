647fc9fcd2912daf0479cbf506e45555b57de562
srivastavakshitijprofessional@gmail.com <srivastavakshitijprofessional@gmail.com>
2021-06-21 14:02:02 +0530

    Setting Up Pages with next

---
+++ pages/index.js
@@ -0,0 +1,11 @@
+import React,{Component} from 'react';
+
+class Lottery extends Component{
+  render()
+  {
+    return (
+      <h1> Decntralized Lottery Application</h1>
+    )
+  }
+}
+export default Lottery;
